addb39ac4122282edd9362fe25e048b9f8dbf387
ChristieX <christie.ferns36@gmail.com>
2023-11-22 23:21:30 +0530

    updated

--- mechanic_dashboard.php
+++ mechanic_dashboard.php
@@ -3,122 +3,109 @@
 include_once 'db_connect.php';
 include_once 'template.html';
 
-// Check if a button is clicked
-if (isset($_POST['approve'])) {
-    // Handle approval logic here
-    $incident_id = $_POST['approve'];
-    $update_status_query = "UPDATE incident SET status = 'approved' WHERE incident_id = $incident_id";
-    $conn->query($update_status_query);
-}
+// Function to generate incident table based on status
+function generateIncidentTable($status, $result)
+{
+    // Output the table header
+    echo "<h3>$status Incidents</h3>
+          <table>
+              <tr>
+                  <th>Incident ID</th>
+                  <th>Description</th>
+                  <th>Location</th>
+                  <th>Mechanic Name</th>
+                  <th>Customer Name</th>
+                  <th>Status</th>
+              </tr>";
 
-if (isset($_POST['disapprove'])) {
-    // Handle disapproval logic here
-    $incident_id = $_POST['disapprove'];
-    $update_status_query = "UPDATE incident SET status = 'disapproved' WHERE incident_id = $incident_id";
-    $conn->query($update_status_query);
-}
-
-// Check if the user is logged in and get their user_id from the session variable
-session_start();
-if (isset($_SESSION['user_id'])) {
-    $user_id = $_SESSION['user_id'];
-
-    // SQL query to retrieve incident information for the logged-in mechanic with status 'requested' or geolocation_id is present
-    $sql = "SELECT incident.incident_id, incident.description, incident.location, incident.status, incident.geolocation_id, CONCAT_WS(' ', customer.name_first, customer.name_middle, customer.name_last) AS customer_name
-            FROM incident
-            INNER JOIN customer ON incident.customer_id = customer.user_id
-            WHERE (incident.status = 'requested' OR incident.geolocation_id IS NOT NULL) AND incident.mechanic_id = $user_id";
-
-    // Execute the query
-    $result = $conn->query($sql);
-
-    // Output the HTML
-    echo "<!DOCTYPE html>
-    <html lang='en'>
-    <head>
-        <meta charset='UTF-8'>
-        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-        <title>Incident Table</title>
-        <style>
-            table {
-                border-collapse: collapse;
-                width: 100%;
-            }
-
-            th, td {
-                border: 1px solid #dddddd;
-                text-align: left;
-                padding: 8px;
-            }
-
-            th {
-                background-color: #f2f2f2;
-            }
-
-            /* Style for the 'View Location' button */
-            button.view-location {
-                padding: 5px 10px;
-                background-color: #4CAF50;
-                color: white;
-                border: none;
-                border-radius: 3px;
-                text-decoration: none;
-                cursor: pointer;
-            }
-        </style>
-    </head>
-    <body>
-
-        <h2>Incident Table</h2>";
-
-    // Check if there are rows returned from the query
-    if ($result->num_rows > 0) {
-        // Output the table header
-        echo "<table>
-                <tr>
-                    <th>Description</th>
-                    <th>Location</th>
-                    <th>Customer Name</th>
-                    <th>Status</th>
-                    <th>Action</th>
-                </tr>";
-
-        // Output data from each row
-        while ($row = $result->fetch_assoc()) {
-            echo "<form method='post'>";
-            echo "<tr>
-                    <td>{$row['description']}</td>
-                    <td>{$row['location']}";
-
-            // Add a 'View Location' button if geolocation_id is present
-            if (!empty($row['geolocation_id'])) {
-                echo "<button class='view-location' type='button'><a href='view_location.php?incident_id={$row['incident_id']}'>View Location</a></button>";
-            }
-
-            echo "</td>
-                    <td>{$row['customer_name']}</td>
-                    <td>{$row['status']}</td>
-                    <td>";
-
-            // Add buttons to change the status
-            echo "<button type='submit' name='approve' value='{$row['incident_id']}'>Approve</button>
-                  <button type='submit' name='disapprove' value='{$row['incident_id']}'>Disapprove</button>";
-
-            echo "</td></tr>";
-            echo "</form>";
-        }
-
-        echo "</table>";
-    } else {
-        echo "No incident found for the logged-in mechanic.";
+    // Output data from each row
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr>
+                <td>{$row['incident_id']}</td>
+                <td>{$row['description']}</td>
+                <td>{$row['location']}</td>
+                <td>{$row['mechanic_name']}</td>
+                <td>{$row['customer_name']}</td>
+                <td>{$row['status']}</td>
+              </tr>";
     }
 
+    echo "</table>";
+}
+
+// SQL query to retrieve incidents with status 'Requested'
+$sqlRequested = "SELECT incident.incident_id, incident.description, incident.location, incident.status, CONCAT_WS(' ', mechanic.first_name, mechanic.middle_name, mechanic.last_name) AS mechanic_name, CONCAT_WS(' ', customer.name_first, customer.name_middle, customer.name_last) AS customer_name
+        FROM incident
+        LEFT JOIN mechanic ON incident.mechanic_id = mechanic.mechanic_id
+        LEFT JOIN customer ON incident.customer_id = customer.user_id
+        WHERE incident.status = 'Requested'";
+
+// SQL query to retrieve incidents with status 'Accepted'
+$sqlAccepted = "SELECT incident.incident_id, incident.description, incident.location, incident.status, CONCAT_WS(' ', mechanic.first_name, mechanic.middle_name, mechanic.last_name) AS mechanic_name, CONCAT_WS(' ', customer.name_first, customer.name_middle, customer.name_last) AS customer_name
+        FROM incident
+        LEFT JOIN mechanic ON incident.mechanic_id = mechanic.mechanic_id
+        LEFT JOIN customer ON incident.customer_id = customer.user_id
+        WHERE incident.status = 'accepted'";
+
+// SQL query to retrieve incidents with status 'Declined'
+$sqlDeclined = "SELECT incident.incident_id, incident.description, incident.location, incident.status, CONCAT_WS(' ', mechanic.first_name, mechanic.middle_name, mechanic.last_name) AS mechanic_name, CONCAT_WS(' ', customer.name_first, customer.name_middle, customer.name_last) AS customer_name
+        FROM incident
+        LEFT JOIN mechanic ON incident.mechanic_id = mechanic.mechanic_id
+        LEFT JOIN customer ON incident.customer_id = customer.user_id
+        WHERE incident.status = 'declined'";
+
+// Execute the queries
+$resultRequested = $conn->query($sqlRequested);
+$resultAccepted = $conn->query($sqlAccepted);
+$resultDeclined = $conn->query($sqlDeclined);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Incident Tables</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+            margin-bottom: 20px;
+        }
+
+        th, td {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
+</head>
+<body>
+
+    <h2>Incident Tables</h2>
+
+    <!-- Navigation Index -->
+    <div>
+        <p><a href="#requested">Requested Incidents</a> | <a href="#accepted">Accepted Incidents</a> | <a href="#declined">Declined Incidents</a></p>
+    </div>
+
+    <!-- Requested Incidents Table -->
+    <?php generateIncidentTable('Requested', $resultRequested); ?>
+
+    <!-- Accepted Incidents Table -->
+    <?php generateIncidentTable('Accepted', $resultAccepted); ?>
+
+    <!-- Declined Incidents Table -->
+    <?php generateIncidentTable('Declined', $resultDeclined); ?>
+
+    <?php
     // Close the database connection
     $conn->close();
+    ?>
 
-    echo "</body>
-    </html>";
-} else {
-    echo "User not logged in.";
-}
-?>
+</body>
+</html>
