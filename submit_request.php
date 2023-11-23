5f01f3ba80d9304a6baab6b0a40baedbc5aeac9b
ChristieX <christie.ferns36@gmail.com>
2023-11-23 07:02:16 +0530

    Update submit_request.php

--- submit_request.php
+++ submit_request.php
@@ -19,6 +19,7 @@ if (isset($_SESSION['user_id'])) {
             // Insert the data into the 'incident' table
             $insertSql = "INSERT INTO incident (customer_id, mechanic_id) VALUES ('$userId', '$mechanicId')";
             $conn->query($insertSql);
+            $_SESSION['incident_id'] = $conn->insert_id;
 
             // You can add additional logic or redirection here if needed
         } else {
@@ -33,6 +34,7 @@ if (isset($_SESSION['user_id'])) {
     // Handle the case where the user is not logged in or the user ID is not available in the session
     echo "User not logged in or user ID not available.";
 }
+header("Location: assistance.php");
 
 // Close the database connection
 $conn->close();
